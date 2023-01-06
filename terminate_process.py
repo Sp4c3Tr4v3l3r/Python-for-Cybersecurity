19ed4922b28691741fe7c8927afc83b90c60c5dc
Sp4c3Tr4v3l3r <59577468+Sp4c3Tr4v3l3r@users.noreply.github.com>
2023-01-05 19:11:11 -0800

    Create terminate_process.py

---
+++ terminate_process.py
@@ -0,0 +1,16 @@
+## This code takes a user input of a process name and then search the list of active processes for that process name.
+## If it finds the process it terminates it using the SIGTERM signal.
+
+import psutil,os,signal
+
+def close_process(processes_to_terminate):
+    for process in psutil.process_iter():
+        if processes_to_terminate in process.name():
+            try:
+                print(f"\nProcess named {processes_to_terminate} with PID {process.pid} was terminated")
+                os.kill(process.pid, signal.SIGTERM)
+            except:
+                continue
+
+processes_to_terminate = input("Input the name of the application you want to close: ")
+close_process(processes_to_terminate)
